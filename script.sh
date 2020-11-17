4976a769bc77a4f4b172c0b6a5de50d61ce2b3c1
brpierre <brieuc.pierre@student.uclouvain.be>
2020-11-17 18:25:42 +0100

    upgrade

--- script.sh
+++ script.sh
@@ -4,19 +4,19 @@ let NTHREAD=$(nproc)*2
 declare -i one
 declare -i two
 
-echo "nb_coeur,temps_CC,temps_P,temps_PC,temps_RW"
+echo "nb_coeur,temps_CC,temps_P,temps_PC,temps_RW,N1,N2"
 
 for ((i=0;i<$RUNS;i++))
 do
 	for ((n=1;n<=$NTHREAD;n++))
 	do
 		one=n/2
-		two=n/2+1
+		two=(n-1)/2+1
 		make clean -s 2> /dev/null
 		output1=$((/usr/bin/time -f %e make -j ${n} -s 2>&1)| cut -d\) -f2)
 		output2=$((/usr/bin/time -f %e ./main -P ${n} -j ${n} -s 2>&1)| cut -d\) -f2)
 		output3=$((/usr/bin/time -f %e ./main -PC ${one} ${two} -j ${n} -s 2>&1)| cut -d\) -f2)
 		output4=$((/usr/bin/time -f %e ./main -RW ${one} ${two} -j ${n} -s 2>&1)| cut -d\) -f2)
-		echo ${n},${output1},${output2},${output3},${output4}
+		echo ${n},${output1},${output2},${output3},${output4},${one},${two}
 	done
 done
