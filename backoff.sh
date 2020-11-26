f6feffb6b36dcd8bf0bcd47a2bcbc9e73745c855
brpierre <brieuc.pierre@student.uclouvain.be>
2020-11-26 18:30:35 +0100

    backoff

--- backoff.sh
+++ backoff.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/bash
-RUNS=1000
+RUNS=20
 Times=1000
 declare -i min
-declare -i max
+declare -i max1
+declare -i max2
+declare -i max3
 
-echo "Vmin,empty,BTTS_1,BTTS_2,BTTS_3"
+echo "Vmin,empty,BTTS_1,BTTS_2,BTTS_3,min,max1,max2,max3"
 
 for ((i=0;i<$RUNS;i++))
 do
-	for ((i=0;i<$Times;i+=10))
+	for ((k=0;k<$Times;k+=10))
 	do
-		min=${i}
-		max=${i}
-		output=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j ${n} -s 2>&1)| cut -d\) -f2)
-		max=${i}*100
-		output1=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j ${n} -s 2>&1)| cut -d\) -f2)
-		max=${i}*100000
-		output2=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j ${n} -s 2>&1)| cut -d\) -f2)
-		echo ${i},0,${output},${output1},${output2}
+		min=${k}
+		max1=${k}*2
+		output=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j 8 -s 2>&1)| cut -d\) -f2)
+		max2=${k}*100000
+		output1=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j 8 -s 2>&1)| cut -d\) -f2)
+		max3=${k}*100000000000
+		output2=$((/usr/bin/time -f %e ./main -BTTS 8 ${min} ${max} -j 8 -s 2>&1)| cut -d\) -f2)
+		echo ${k},0,${output},${output1},${output2},${min},${max1},${max2},${max3}
 	done
 done
