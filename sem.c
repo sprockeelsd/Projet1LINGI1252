bba11302467593532c23c59e2467493aeee63a9f
brpierre <62648586+brpierre@users.noreply.github.com>
2020-11-28 17:53:51 +0100

    Update sem.c

--- sem.c
+++ sem.c
@@ -1,31 +1,31 @@
 #include "header.h"
 
 void* post(semaphore *arg){
-	lock_TS(arg->mutexP); //un thread modifie current a la fois //b sem.c:4
-	while(arg->current==arg->max); //pas de threads actifs //b sem.c:5
-	lock_TS(arg->protection); //un thread modifie current a la fois
+	lock_TS(arg->mutexP); //b sem.c:4 (pour le debug)
+	while(arg->current==arg->max); //pas de threads actifs
+	lock_TS(arg->protection); 
 	arg->current = arg->current+1; //une place en plus de libre
 	unlock(arg->protection); //next thread peut entrer 
-	unlock(arg->mutexP); //next post can enter
+	unlock(arg->mutexP); //next post peut entrer
 }
 
 void* wait(semaphore *arg){
-	lock_TS(arg->mutexW); //un thread modifie current a la fois
+	lock_TS(arg->mutexW); 
 	while(arg->current==0); //plus de places libres
-	lock_TS(arg->protection); //un thread modifie current a la fois
+	lock_TS(arg->protection);
 	arg->current = arg->current-1; //une place en moins de libre
 	unlock(arg->protection); //next thread peut entrer 
-	unlock(arg->mutexW); //next wait can enter
+	unlock(arg->mutexW); //next wait peut entrer
 }
 
-semaphore* init_S(int start, int maxi){//start is either 0 or maxi
+semaphore* init_S(int start, int maxi){//normalement start vaut soit 0 soit maxi
 	semaphore* arg = malloc(sizeof(semaphore));
 	//init local
-	arg->current = start;
-	arg->max = maxi;
-	arg->mutexW = init();
-	arg->mutexP = init();
-	arg->protection = init();
+	arg->current = start; //l'état de la sémaphore
+	arg->max = maxi; //valeur pour emêcher de faire trop d'appels a post
+	arg->mutexW = init(); //un thread rentre dans wait à la fois
+	arg->mutexP = init(); //un thread rentre dans post à la fois
+	arg->protection = init(); //un thread modifie l'état d'un sémaphore à la fois
 	return arg;
 }
 
